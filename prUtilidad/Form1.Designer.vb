1e58b8e5882e3f6cf2cbe29452f694344ea00bfe
JuanBustos <juanantoniobustos@outlook.es>
2019-06-28 17:18:39 +0200

    Creación proyecto prUtilidad

---
+++ prUtilidad/Form1.Designer.vb
@@ -0,0 +1,173 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtSalario = New System.Windows.Forms.TextBox()
+        Me.btnNuevo = New System.Windows.Forms.Button()
+        Me.btnCalcular = New System.Windows.Forms.Button()
+        Me.btnSalir = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtUtilidad = New System.Windows.Forms.TextBox()
+        Me.cboxAntiguedad = New System.Windows.Forms.ComboBox()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.cboxAntiguedad)
+        Me.GroupBox1.Controls.Add(Me.btnSalir)
+        Me.GroupBox1.Controls.Add(Me.btnCalcular)
+        Me.GroupBox1.Controls.Add(Me.btnNuevo)
+        Me.GroupBox1.Controls.Add(Me.txtSalario)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Location = New System.Drawing.Point(13, 13)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(619, 196)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Datos Ingresados"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.txtUtilidad)
+        Me.GroupBox2.Controls.Add(Me.Label3)
+        Me.GroupBox2.Location = New System.Drawing.Point(13, 216)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(619, 201)
+        Me.GroupBox2.TabIndex = 1
+        Me.GroupBox2.TabStop = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(44, 35)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(42, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Salario:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(44, 94)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(64, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Antigüedad:"
+        '
+        'txtSalario
+        '
+        Me.txtSalario.Location = New System.Drawing.Point(131, 32)
+        Me.txtSalario.Name = "txtSalario"
+        Me.txtSalario.Size = New System.Drawing.Size(121, 20)
+        Me.txtSalario.TabIndex = 2
+        '
+        'btnNuevo
+        '
+        Me.btnNuevo.Location = New System.Drawing.Point(146, 152)
+        Me.btnNuevo.Name = "btnNuevo"
+        Me.btnNuevo.Size = New System.Drawing.Size(75, 23)
+        Me.btnNuevo.TabIndex = 4
+        Me.btnNuevo.Text = "Nuevo"
+        Me.btnNuevo.UseVisualStyleBackColor = True
+        '
+        'btnCalcular
+        '
+        Me.btnCalcular.Location = New System.Drawing.Point(272, 152)
+        Me.btnCalcular.Name = "btnCalcular"
+        Me.btnCalcular.Size = New System.Drawing.Size(75, 23)
+        Me.btnCalcular.TabIndex = 5
+        Me.btnCalcular.Text = "&Calcular"
+        Me.btnCalcular.UseVisualStyleBackColor = True
+        '
+        'btnSalir
+        '
+        Me.btnSalir.Location = New System.Drawing.Point(400, 152)
+        Me.btnSalir.Name = "btnSalir"
+        Me.btnSalir.Size = New System.Drawing.Size(75, 23)
+        Me.btnSalir.TabIndex = 6
+        Me.btnSalir.Text = "Salir"
+        Me.btnSalir.UseVisualStyleBackColor = True
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(44, 90)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(45, 13)
+        Me.Label3.TabIndex = 0
+        Me.Label3.Text = "Utilidad:"
+        '
+        'txtUtilidad
+        '
+        Me.txtUtilidad.Location = New System.Drawing.Point(131, 87)
+        Me.txtUtilidad.Name = "txtUtilidad"
+        Me.txtUtilidad.Size = New System.Drawing.Size(121, 20)
+        Me.txtUtilidad.TabIndex = 1
+        '
+        'cboxAntiguedad
+        '
+        Me.cboxAntiguedad.FormattingEnabled = True
+        Me.cboxAntiguedad.Items.AddRange(New Object() {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"})
+        Me.cboxAntiguedad.Location = New System.Drawing.Point(131, 91)
+        Me.cboxAntiguedad.Name = "cboxAntiguedad"
+        Me.cboxAntiguedad.Size = New System.Drawing.Size(121, 21)
+        Me.cboxAntiguedad.TabIndex = 7
+        Me.cboxAntiguedad.Text = "0"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.White
+        Me.ClientSize = New System.Drawing.Size(645, 450)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Form1"
+        Me.Text = ".:. Reparto de Utilidades .:."
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents cboxAntiguedad As ComboBox
+    Friend WithEvents btnSalir As Button
+    Friend WithEvents btnCalcular As Button
+    Friend WithEvents btnNuevo As Button
+    Friend WithEvents txtSalario As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents txtUtilidad As TextBox
+    Friend WithEvents Label3 As Label
+End Class
---
+++ prUtilidad/Form1.vb
@@ -0,0 +1,41 @@
+﻿Public Class Form1
+    Private Sub BtnCalcular_Click(sender As Object, e As EventArgs) Handles btnCalcular.Click
+        'Declarando variables
+        Dim salario As Double
+        Dim antiguedad As Byte
+        Dim utilidad As Double
+
+        'Entrada de datos
+        salario = Val(txtSalario.Text)
+        antiguedad = Val(cboxAntiguedad.Text)
+
+        'Proceso
+        If (antiguedad < 1) Then
+            utilidad = salario * 5 / 100
+        ElseIf (antiguedad >= 1 And antiguedad < 2) Then
+            utilidad = salario * 7 / 100
+        ElseIf (antiguedad >= 2 And antiguedad < 5) Then
+            utilidad = salario * 10 / 100
+        ElseIf (antiguedad >= 5 And antiguedad < 10) Then
+            utilidad = salario * 15 / 100
+        ElseIf (antiguedad >= 10) Then
+            utilidad = salario * 20 / 100
+        End If
+
+        'Salida de Información
+        txtUtilidad.Text = utilidad
+
+    End Sub
+
+    Private Sub BtnNuevo_Click(sender As Object, e As EventArgs) Handles btnNuevo.Click
+        txtSalario.Clear()
+        cboxAntiguedad.Text = 0
+        txtUtilidad.Clear()
+
+    End Sub
+
+    Private Sub BtnSalir_Click(sender As Object, e As EventArgs) Handles btnSalir.Click
+        End
+
+    End Sub
+End Class

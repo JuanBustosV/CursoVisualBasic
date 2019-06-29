11154eb539d4428ca37c53db4aaebfbb1f016418
JuanBustosV <juanbv81@gmail.com>
2019-06-29 19:22:07 +0200

    Proyecto prTimer desarrollado

---
+++ prTimer/Form1.Designer.vb
@@ -0,0 +1,120 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.btnPlay = New System.Windows.Forms.Button()
+        Me.btnStop = New System.Windows.Forms.Button()
+        Me.lblFecha = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("BankGothic Md BT", 26.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.Red
+        Me.Label1.Location = New System.Drawing.Point(142, 138)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(238, 36)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "IncanatoIT"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("BankGothic Md BT", 26.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.Color.Green
+        Me.Label2.Location = New System.Drawing.Point(142, 138)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(238, 36)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "IncanatoIT"
+        '
+        'btnPlay
+        '
+        Me.btnPlay.ForeColor = System.Drawing.Color.Red
+        Me.btnPlay.Location = New System.Drawing.Point(432, 51)
+        Me.btnPlay.Name = "btnPlay"
+        Me.btnPlay.Size = New System.Drawing.Size(75, 40)
+        Me.btnPlay.TabIndex = 2
+        Me.btnPlay.Text = "Play"
+        Me.btnPlay.UseVisualStyleBackColor = True
+        '
+        'btnStop
+        '
+        Me.btnStop.ForeColor = System.Drawing.Color.Red
+        Me.btnStop.Location = New System.Drawing.Point(432, 247)
+        Me.btnStop.Name = "btnStop"
+        Me.btnStop.Size = New System.Drawing.Size(75, 40)
+        Me.btnStop.TabIndex = 3
+        Me.btnStop.Text = "Stop"
+        Me.btnStop.UseVisualStyleBackColor = True
+        '
+        'lblFecha
+        '
+        Me.lblFecha.AutoSize = True
+        Me.lblFecha.Location = New System.Drawing.Point(31, 13)
+        Me.lblFecha.Name = "lblFecha"
+        Me.lblFecha.Size = New System.Drawing.Size(39, 13)
+        Me.lblFecha.TabIndex = 4
+        Me.lblFecha.Text = "Label3"
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 500
+        '
+        'Timer2
+        '
+        Me.Timer2.Enabled = True
+        Me.Timer2.Interval = 1000
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.White
+        Me.ClientSize = New System.Drawing.Size(536, 372)
+        Me.Controls.Add(Me.lblFecha)
+        Me.Controls.Add(Me.btnStop)
+        Me.Controls.Add(Me.btnPlay)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = ".:. Control Timer .:."
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents btnPlay As Button
+    Friend WithEvents btnStop As Button
+    Friend WithEvents lblFecha As Label
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Timer2 As Timer
+End Class
---
+++ prTimer/Form1.vb
@@ -0,0 +1,35 @@
+﻿Public Class Form1
+
+    Private num As Integer = 0
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        If (num Mod 2 = 0) Then
+            Label1.Visible = True
+            Label2.Visible = False
+        Else
+            Label1.Visible = False
+            Label2.Visible = True
+        End If
+
+        num = num + 1
+    End Sub
+
+    Private Sub BtnPlay_Click(sender As Object, e As EventArgs) Handles btnPlay.Click
+        Timer1.Enabled = True
+    End Sub
+
+    Private Sub BtnStop_Click(sender As Object, e As EventArgs) Handles btnStop.Click
+        Timer1.Enabled = False
+
+        num = 0
+    End Sub
+
+    Private Sub Timer2_Tick(sender As Object, e As EventArgs) Handles Timer2.Tick
+        lblFecha.Text = TimeOfDay
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        lblFecha.Text = TimeOfDay
+    End Sub
+End Class

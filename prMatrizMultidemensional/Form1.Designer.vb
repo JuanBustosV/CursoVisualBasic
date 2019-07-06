d4ca485021c2cd0541ab61afc409ee76c81f415d
JuanBustosV <juanbv81@gmail.com>
2019-07-06 23:35:26 +0200

    Matriz, vector multidimensional

---
+++ prMatrizMultidemensional/Form1.Designer.vb
@@ -0,0 +1,242 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtCantPlatos = New System.Windows.Forms.TextBox()
+        Me.btnCargar = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtPlato = New System.Windows.Forms.TextBox()
+        Me.txtPrecio = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txtDisponible = New System.Windows.Forms.TextBox()
+        Me.btnBuscar = New System.Windows.Forms.Button()
+        Me.GroupBox3 = New System.Windows.Forms.GroupBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.txtCantidad = New System.Windows.Forms.TextBox()
+        Me.btnVender = New System.Windows.Forms.Button()
+        Me.btnSalir = New System.Windows.Forms.Button()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.GroupBox3.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.btnCargar)
+        Me.GroupBox1.Controls.Add(Me.txtCantPlatos)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Location = New System.Drawing.Point(13, 13)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(493, 99)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Ingresar Datos"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.GroupBox3)
+        Me.GroupBox2.Controls.Add(Me.btnBuscar)
+        Me.GroupBox2.Controls.Add(Me.txtDisponible)
+        Me.GroupBox2.Controls.Add(Me.Label4)
+        Me.GroupBox2.Controls.Add(Me.txtPrecio)
+        Me.GroupBox2.Controls.Add(Me.txtPlato)
+        Me.GroupBox2.Controls.Add(Me.Label3)
+        Me.GroupBox2.Controls.Add(Me.Label2)
+        Me.GroupBox2.Location = New System.Drawing.Point(13, 118)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(493, 292)
+        Me.GroupBox2.TabIndex = 1
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Buscar"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(28, 44)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(84, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Cantidad Platos:"
+        '
+        'txtCantPlatos
+        '
+        Me.txtCantPlatos.Location = New System.Drawing.Point(141, 41)
+        Me.txtCantPlatos.Name = "txtCantPlatos"
+        Me.txtCantPlatos.Size = New System.Drawing.Size(100, 20)
+        Me.txtCantPlatos.TabIndex = 1
+        '
+        'btnCargar
+        '
+        Me.btnCargar.Location = New System.Drawing.Point(309, 39)
+        Me.btnCargar.Name = "btnCargar"
+        Me.btnCargar.Size = New System.Drawing.Size(75, 23)
+        Me.btnCargar.TabIndex = 2
+        Me.btnCargar.Text = "Cargar"
+        Me.btnCargar.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(28, 46)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(34, 13)
+        Me.Label2.TabIndex = 0
+        Me.Label2.Text = "Plato:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(28, 92)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(40, 13)
+        Me.Label3.TabIndex = 1
+        Me.Label3.Text = "Precio:"
+        '
+        'txtPlato
+        '
+        Me.txtPlato.Location = New System.Drawing.Point(77, 43)
+        Me.txtPlato.Name = "txtPlato"
+        Me.txtPlato.Size = New System.Drawing.Size(100, 20)
+        Me.txtPlato.TabIndex = 2
+        '
+        'txtPrecio
+        '
+        Me.txtPrecio.Location = New System.Drawing.Point(77, 89)
+        Me.txtPrecio.Name = "txtPrecio"
+        Me.txtPrecio.Size = New System.Drawing.Size(100, 20)
+        Me.txtPrecio.TabIndex = 3
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(239, 92)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(64, 13)
+        Me.Label4.TabIndex = 4
+        Me.Label4.Text = "Disponibles:"
+        '
+        'txtDisponible
+        '
+        Me.txtDisponible.Location = New System.Drawing.Point(319, 89)
+        Me.txtDisponible.Name = "txtDisponible"
+        Me.txtDisponible.Size = New System.Drawing.Size(100, 20)
+        Me.txtDisponible.TabIndex = 5
+        '
+        'btnBuscar
+        '
+        Me.btnBuscar.Location = New System.Drawing.Point(344, 46)
+        Me.btnBuscar.Name = "btnBuscar"
+        Me.btnBuscar.Size = New System.Drawing.Size(75, 23)
+        Me.btnBuscar.TabIndex = 6
+        Me.btnBuscar.Text = "Buscar"
+        Me.btnBuscar.UseVisualStyleBackColor = True
+        '
+        'GroupBox3
+        '
+        Me.GroupBox3.Controls.Add(Me.btnSalir)
+        Me.GroupBox3.Controls.Add(Me.btnVender)
+        Me.GroupBox3.Controls.Add(Me.txtCantidad)
+        Me.GroupBox3.Controls.Add(Me.Label5)
+        Me.GroupBox3.Location = New System.Drawing.Point(16, 152)
+        Me.GroupBox3.Name = "GroupBox3"
+        Me.GroupBox3.Size = New System.Drawing.Size(459, 117)
+        Me.GroupBox3.TabIndex = 7
+        Me.GroupBox3.TabStop = False
+        Me.GroupBox3.Text = "Vender"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(44, 47)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(52, 13)
+        Me.Label5.TabIndex = 0
+        Me.Label5.Text = "Cantidad:"
+        '
+        'txtCantidad
+        '
+        Me.txtCantidad.Location = New System.Drawing.Point(125, 44)
+        Me.txtCantidad.Name = "txtCantidad"
+        Me.txtCantidad.Size = New System.Drawing.Size(100, 20)
+        Me.txtCantidad.TabIndex = 1
+        '
+        'btnVender
+        '
+        Me.btnVender.Location = New System.Drawing.Point(280, 19)
+        Me.btnVender.Name = "btnVender"
+        Me.btnVender.Size = New System.Drawing.Size(75, 23)
+        Me.btnVender.TabIndex = 2
+        Me.btnVender.Text = "Vender"
+        Me.btnVender.UseVisualStyleBackColor = True
+        '
+        'btnSalir
+        '
+        Me.btnSalir.Location = New System.Drawing.Point(280, 74)
+        Me.btnSalir.Name = "btnSalir"
+        Me.btnSalir.Size = New System.Drawing.Size(75, 23)
+        Me.btnSalir.TabIndex = 3
+        Me.btnSalir.Text = "Salir"
+        Me.btnSalir.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.White
+        Me.ClientSize = New System.Drawing.Size(525, 423)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Form1"
+        Me.Text = ".:. Matriz Multidimensional .:."
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.GroupBox3.ResumeLayout(False)
+        Me.GroupBox3.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents btnCargar As Button
+    Friend WithEvents txtCantPlatos As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents GroupBox3 As GroupBox
+    Friend WithEvents btnSalir As Button
+    Friend WithEvents btnVender As Button
+    Friend WithEvents txtCantidad As TextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents btnBuscar As Button
+    Friend WithEvents txtDisponible As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents txtPrecio As TextBox
+    Friend WithEvents txtPlato As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+End Class
---
+++ prMatrizMultidemensional/Form1.vb
@@ -0,0 +1,71 @@
+﻿Public Class Form1
+    Private mPlatos(,) As String
+    Private cantPlatos As String
+    Private index As Byte
+    Private encuentra As Byte = 0
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        btnVender.Enabled = False
+        txtPrecio.Enabled = False
+        txtDisponible.Enabled = False
+    End Sub
+
+    Private Sub BtnCargar_Click(sender As Object, e As EventArgs) Handles btnCargar.Click
+        'Entrada de datos
+        cantPlatos = Val(txtCantPlatos.Text)
+        'Inicializar la matriz
+        ReDim Preserve mPlatos(cantPlatos, 3)
+
+        'Ingresar los datos a la matriz
+        For i As Integer = 0 To (cantPlatos - 1) Step 1
+            mPlatos(i, 0) = InputBox("Ingrese nombre del plato " & (i + 1), "Restaurant")
+            mPlatos(i, 1) = InputBox("Ingrese precio del plato " & (i + 1), "Restaurant")
+            mPlatos(i, 2) = InputBox("Ingrese stock del plato " & (i + 1), "Restaurant")
+        Next
+    End Sub
+
+    Private Sub BtnBuscar_Click(sender As Object, e As EventArgs) Handles btnBuscar.Click
+        'Declaración de variables
+        Dim plato As String
+        'Entrada de datos
+        plato = txtPlato.Text
+        'Buscamos el plato en la matriz
+        For i As Integer = 0 To (cantPlatos - 1) Step 1
+            If (mPlatos(i, 0) = plato) Then
+                encuentra = 1
+                txtPrecio.Text = mPlatos(i, 1)
+                txtDisponible.Text = mPlatos(i, 2)
+                index = i
+                btnVender.Enabled = True
+            End If
+        Next
+        'si no encuentra el plato en la matriz
+        If (encuentra = 0) Then
+            MessageBox.Show("No existe el plato", "Restaurant", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            btnVender.Enabled = False
+        End If
+
+    End Sub
+
+    Private Sub BtnSalir_Click(sender As Object, e As EventArgs) Handles btnSalir.Click
+        End
+
+    End Sub
+
+    Private Sub BtnVender_Click(sender As Object, e As EventArgs) Handles btnVender.Click
+        'Declaración de variables
+        Dim cant As Integer
+        Dim stock As Integer
+        'Entrada de datos
+        cant = Val(txtCantidad.Text)
+        stock = mPlatos(index, 2)
+
+        If (stock >= cant) Then
+            'Disminuimos la cantidad de stock
+            mPlatos(index, 2) -= cant
+            txtDisponible.Text = mPlatos(index, 2)
+        Else
+            MessageBox.Show("No hay suficiente stock", "Restaurant", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End If
+    End Sub
+End Class

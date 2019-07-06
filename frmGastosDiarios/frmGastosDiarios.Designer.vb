5346ee5e13e41ce347d3d31355b13b55ea775e77
JuanBustosV <juanbv81@gmail.com>
2019-07-06 19:38:14 +0200

    Nuevo form usando vector -ReDim Preserve

---
+++ frmGastosDiarios/frmGastosDiarios.Designer.vb
@@ -0,0 +1,204 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmGastosDiarios
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtCantDias = New System.Windows.Forms.TextBox()
+        Me.btnIngresar = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.cboGastos = New System.Windows.Forms.ComboBox()
+        Me.txtTotalGastos = New System.Windows.Forms.TextBox()
+        Me.txtDiaMayor = New System.Windows.Forms.TextBox()
+        Me.txtGastoMayor = New System.Windows.Forms.TextBox()
+        Me.btnNuevo = New System.Windows.Forms.Button()
+        Me.btnSalir = New System.Windows.Forms.Button()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.btnIngresar)
+        Me.GroupBox1.Controls.Add(Me.txtCantDias)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Location = New System.Drawing.Point(13, 13)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(390, 189)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Ingrese Datos"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.btnSalir)
+        Me.GroupBox2.Controls.Add(Me.btnNuevo)
+        Me.GroupBox2.Controls.Add(Me.txtGastoMayor)
+        Me.GroupBox2.Controls.Add(Me.txtDiaMayor)
+        Me.GroupBox2.Controls.Add(Me.txtTotalGastos)
+        Me.GroupBox2.Controls.Add(Me.cboGastos)
+        Me.GroupBox2.Controls.Add(Me.Label4)
+        Me.GroupBox2.Controls.Add(Me.Label3)
+        Me.GroupBox2.Controls.Add(Me.Label2)
+        Me.GroupBox2.Location = New System.Drawing.Point(12, 208)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(391, 230)
+        Me.GroupBox2.TabIndex = 1
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Resultados"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(19, 81)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(78, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Cantidad Días:"
+        '
+        'txtCantDias
+        '
+        Me.txtCantDias.Location = New System.Drawing.Point(129, 78)
+        Me.txtCantDias.Name = "txtCantDias"
+        Me.txtCantDias.Size = New System.Drawing.Size(100, 20)
+        Me.txtCantDias.TabIndex = 1
+        '
+        'btnIngresar
+        '
+        Me.btnIngresar.Location = New System.Drawing.Point(271, 76)
+        Me.btnIngresar.Name = "btnIngresar"
+        Me.btnIngresar.Size = New System.Drawing.Size(75, 23)
+        Me.btnIngresar.TabIndex = 2
+        Me.btnIngresar.Text = "Ingresar"
+        Me.btnIngresar.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(28, 51)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(43, 13)
+        Me.Label2.TabIndex = 0
+        Me.Label2.Text = "Gastos:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(28, 99)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(70, 13)
+        Me.Label3.TabIndex = 1
+        Me.Label3.Text = "Total Gastos:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(28, 151)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(70, 13)
+        Me.Label4.TabIndex = 2
+        Me.Label4.Text = "Mayor Gasto:"
+        '
+        'cboGastos
+        '
+        Me.cboGastos.FormattingEnabled = True
+        Me.cboGastos.Location = New System.Drawing.Point(130, 48)
+        Me.cboGastos.Name = "cboGastos"
+        Me.cboGastos.Size = New System.Drawing.Size(121, 21)
+        Me.cboGastos.TabIndex = 3
+        '
+        'txtTotalGastos
+        '
+        Me.txtTotalGastos.Location = New System.Drawing.Point(151, 96)
+        Me.txtTotalGastos.Name = "txtTotalGastos"
+        Me.txtTotalGastos.Size = New System.Drawing.Size(100, 20)
+        Me.txtTotalGastos.TabIndex = 4
+        '
+        'txtDiaMayor
+        '
+        Me.txtDiaMayor.Location = New System.Drawing.Point(130, 148)
+        Me.txtDiaMayor.Name = "txtDiaMayor"
+        Me.txtDiaMayor.Size = New System.Drawing.Size(100, 20)
+        Me.txtDiaMayor.TabIndex = 5
+        '
+        'txtGastoMayor
+        '
+        Me.txtGastoMayor.Location = New System.Drawing.Point(272, 148)
+        Me.txtGastoMayor.Name = "txtGastoMayor"
+        Me.txtGastoMayor.Size = New System.Drawing.Size(100, 20)
+        Me.txtGastoMayor.TabIndex = 6
+        '
+        'btnNuevo
+        '
+        Me.btnNuevo.Location = New System.Drawing.Point(43, 190)
+        Me.btnNuevo.Name = "btnNuevo"
+        Me.btnNuevo.Size = New System.Drawing.Size(75, 23)
+        Me.btnNuevo.TabIndex = 7
+        Me.btnNuevo.Text = "Nuevo"
+        Me.btnNuevo.UseVisualStyleBackColor = True
+        '
+        'btnSalir
+        '
+        Me.btnSalir.Location = New System.Drawing.Point(257, 190)
+        Me.btnSalir.Name = "btnSalir"
+        Me.btnSalir.Size = New System.Drawing.Size(75, 23)
+        Me.btnSalir.TabIndex = 8
+        Me.btnSalir.Text = "Salir"
+        Me.btnSalir.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.White
+        Me.ClientSize = New System.Drawing.Size(418, 450)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Form1"
+        Me.Text = ".:: Gastos Diarios ::."
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents btnIngresar As Button
+    Friend WithEvents txtCantDias As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents btnSalir As Button
+    Friend WithEvents btnNuevo As Button
+    Friend WithEvents txtGastoMayor As TextBox
+    Friend WithEvents txtDiaMayor As TextBox
+    Friend WithEvents txtTotalGastos As TextBox
+    Friend WithEvents cboGastos As ComboBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+End Class
---
+++ frmGastosDiarios/frmGastosDiarios.vb
@@ -0,0 +1,51 @@
+﻿Public Class frmGastosDiarios
+    Private Sub BtnSalir_Click(sender As Object, e As EventArgs) Handles btnSalir.Click
+        End
+
+    End Sub
+
+    Private Sub BtnIngresar_Click(sender As Object, e As EventArgs) Handles btnIngresar.Click
+        'Declaración de variables
+        Dim vGastos() As Double
+        Dim gasto As Double, totalGasto As Double, diaMayor As Integer
+        Dim gastoMayor As Double
+        Dim cantDias As Integer
+
+        'Inicializar variables
+        totalGasto = 0
+        diaMayor = 1
+        gastoMayor = 0
+
+        'Leer los gastos
+        cantDias = Val(txtCantDias.Text)
+        ReDim Preserve vGastos(cantDias)    'Establece el tamaño del vector
+        For i As Integer = 0 To (cantDias - 1) Step 1
+            gasto = InputBox("Ingrese el gasto del día " & (i + 1), "Gastos")
+            vGastos(i) = gasto
+        Next
+
+        For j As Integer = 0 To (cantDias - 1) Step 1
+            If (vGastos(j) > gastoMayor) Then
+                gastoMayor = vGastos(j)
+                diaMayor = j + 1
+            End If
+            cboGastos.Items.Add("Día " & (j + 1) & " Total: " & vGastos(j))
+            totalGasto += vGastos(j)
+        Next
+        ' Salida de Información
+        txtGastoMayor.Text = gastoMayor
+        txtDiaMayor.Text = "Día: " & diaMayor
+        txtTotalGastos.Text = totalGasto
+
+    End Sub
+
+    Private Sub BtnNuevo_Click(sender As Object, e As EventArgs) Handles btnNuevo.Click
+        cboGastos.Items.Clear()
+        txtCantDias.Clear()
+        txtGastoMayor.Clear()
+        txtDiaMayor.Clear()
+        txtTotalGastos.Clear()
+
+
+    End Sub
+End Class

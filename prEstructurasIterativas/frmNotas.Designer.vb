ab98d66695ce1833017333bc2f78d187277d250a
JuanBustosV <juanbv81@gmail.com>
2019-07-06 11:15:10 +0200

    Nuevo proyecto uso bucle For

---
+++ prEstructurasIterativas/frmNotas.Designer.vb
@@ -0,0 +1,171 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmNotas
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.txtNumero = New System.Windows.Forms.TextBox()
+        Me.btnSalir = New System.Windows.Forms.Button()
+        Me.btnCalcular = New System.Windows.Forms.Button()
+        Me.btnNuevo = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtPromedio = New System.Windows.Forms.TextBox()
+        Me.txtNotaBaja = New System.Windows.Forms.TextBox()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.txtNumero)
+        Me.GroupBox1.Controls.Add(Me.btnSalir)
+        Me.GroupBox1.Controls.Add(Me.btnCalcular)
+        Me.GroupBox1.Controls.Add(Me.btnNuevo)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Location = New System.Drawing.Point(24, 13)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(575, 191)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Ingrese Alumnos"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.txtNotaBaja)
+        Me.GroupBox2.Controls.Add(Me.txtPromedio)
+        Me.GroupBox2.Controls.Add(Me.Label3)
+        Me.GroupBox2.Controls.Add(Me.Label2)
+        Me.GroupBox2.Location = New System.Drawing.Point(24, 219)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(575, 219)
+        Me.GroupBox2.TabIndex = 1
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Resultados"
+        '
+        'txtNumero
+        '
+        Me.txtNumero.Location = New System.Drawing.Point(197, 61)
+        Me.txtNumero.Name = "txtNumero"
+        Me.txtNumero.Size = New System.Drawing.Size(194, 20)
+        Me.txtNumero.TabIndex = 9
+        '
+        'btnSalir
+        '
+        Me.btnSalir.Location = New System.Drawing.Point(391, 115)
+        Me.btnSalir.Name = "btnSalir"
+        Me.btnSalir.Size = New System.Drawing.Size(95, 23)
+        Me.btnSalir.TabIndex = 8
+        Me.btnSalir.Text = "Salir"
+        Me.btnSalir.UseVisualStyleBackColor = True
+        '
+        'btnCalcular
+        '
+        Me.btnCalcular.Location = New System.Drawing.Point(230, 115)
+        Me.btnCalcular.Name = "btnCalcular"
+        Me.btnCalcular.Size = New System.Drawing.Size(95, 23)
+        Me.btnCalcular.TabIndex = 7
+        Me.btnCalcular.Text = "Calcular"
+        Me.btnCalcular.UseVisualStyleBackColor = True
+        '
+        'btnNuevo
+        '
+        Me.btnNuevo.Location = New System.Drawing.Point(77, 115)
+        Me.btnNuevo.Name = "btnNuevo"
+        Me.btnNuevo.Size = New System.Drawing.Size(95, 23)
+        Me.btnNuevo.TabIndex = 6
+        Me.btnNuevo.Text = "Nuevo"
+        Me.btnNuevo.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(74, 64)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(105, 13)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "Número de Alumnos:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(42, 54)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(54, 13)
+        Me.Label2.TabIndex = 0
+        Me.Label2.Text = "Promedio:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(45, 111)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(79, 13)
+        Me.Label3.TabIndex = 1
+        Me.Label3.Text = "Nota más Baja:"
+        '
+        'txtPromedio
+        '
+        Me.txtPromedio.Location = New System.Drawing.Point(143, 54)
+        Me.txtPromedio.Name = "txtPromedio"
+        Me.txtPromedio.Size = New System.Drawing.Size(100, 20)
+        Me.txtPromedio.TabIndex = 2
+        '
+        'txtNotaBaja
+        '
+        Me.txtNotaBaja.Location = New System.Drawing.Point(143, 111)
+        Me.txtNotaBaja.Name = "txtNotaBaja"
+        Me.txtNotaBaja.Size = New System.Drawing.Size(100, 20)
+        Me.txtNotaBaja.TabIndex = 3
+        '
+        'frmNotas
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.White
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "frmNotas"
+        Me.Text = ".:. Promedio de Notas .:."
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents txtNumero As TextBox
+    Friend WithEvents btnSalir As Button
+    Friend WithEvents btnCalcular As Button
+    Friend WithEvents btnNuevo As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtNotaBaja As TextBox
+    Friend WithEvents txtPromedio As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+End Class
---
+++ prEstructurasIterativas/frmNotas.vb
@@ -0,0 +1,50 @@
+﻿Public Class frmNotas
+    Private Sub BtnCalcular_Click(sender As Object, e As EventArgs) Handles btnCalcular.Click
+        Try
+            'Declaración de variables
+            Dim numAlumnos As Integer
+            Dim nota, sumaNotas, notaBaja, promedio As Double
+
+            If (Val(txtNumero.Text) - Int(Val(txtNumero.Text)) = 0) Then
+                'Entrada de datos
+                numAlumnos = Val(txtNumero.Text)
+                'Inicializamos las variables
+                sumaNotas = 0
+                notaBaja = 20
+
+                For i = 1 To numAlumnos Step 1
+                    nota = InputBox("Ingrese Nota del alumno " & i, "Registro de Notas")
+                    sumaNotas += nota
+
+                    'Evaluo si esa nota es la nota más baja
+                    If (nota < notaBaja) Then
+                        notaBaja = nota
+                    End If
+                Next
+
+                'Calculo promedio
+                promedio = sumaNotas / numAlumnos
+
+                'Salida de la información
+                txtPromedio.Text = promedio
+                txtNotaBaja.Text = notaBaja
+            Else
+                MessageBox.Show("Ingrese un valor entero", "Promedio de Notas", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            End If
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+
+    Private Sub BtnNuevo_Click(sender As Object, e As EventArgs) Handles btnNuevo.Click
+        txtNumero.Clear()
+        txtNotaBaja.Clear()
+        txtPromedio.Clear()
+
+    End Sub
+
+    Private Sub BtnSalir_Click(sender As Object, e As EventArgs) Handles btnSalir.Click
+        Me.Close()
+
+    End Sub
+End Class

6c7f10792507fd5678db61e52a0d0da7dda68a19
JuanBustosV <juanbv81@gmail.com>
2019-07-07 16:55:55 +0200

    Proyecto: Gestión de Ahorros

---
+++ prGestionAhorros/Form1.Designer.vb
@@ -0,0 +1,242 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtCliente = New System.Windows.Forms.TextBox()
+        Me.txtMonto = New System.Windows.Forms.TextBox()
+        Me.btnAbrir = New System.Windows.Forms.Button()
+        Me.btnDepositos = New System.Windows.Forms.Button()
+        Me.btnRetiros = New System.Windows.Forms.Button()
+        Me.GroupBox3 = New System.Windows.Forms.GroupBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.lstDepositos = New System.Windows.Forms.ListBox()
+        Me.lstRetiros = New System.Windows.Forms.ListBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.txtSaldo = New System.Windows.Forms.TextBox()
+        Me.btnLimpiar = New System.Windows.Forms.Button()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.GroupBox3.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.btnAbrir)
+        Me.GroupBox1.Controls.Add(Me.txtMonto)
+        Me.GroupBox1.Controls.Add(Me.txtCliente)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Location = New System.Drawing.Point(13, 13)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(440, 126)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Ingreso de Datos"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.btnRetiros)
+        Me.GroupBox2.Controls.Add(Me.btnDepositos)
+        Me.GroupBox2.Location = New System.Drawing.Point(13, 145)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(440, 61)
+        Me.GroupBox2.TabIndex = 1
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Operaciones"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(35, 42)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(42, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Cliente:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(35, 86)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(40, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Monto:"
+        '
+        'txtCliente
+        '
+        Me.txtCliente.Location = New System.Drawing.Point(123, 39)
+        Me.txtCliente.Name = "txtCliente"
+        Me.txtCliente.Size = New System.Drawing.Size(127, 20)
+        Me.txtCliente.TabIndex = 2
+        '
+        'txtMonto
+        '
+        Me.txtMonto.Location = New System.Drawing.Point(123, 83)
+        Me.txtMonto.Name = "txtMonto"
+        Me.txtMonto.Size = New System.Drawing.Size(100, 20)
+        Me.txtMonto.TabIndex = 3
+        '
+        'btnAbrir
+        '
+        Me.btnAbrir.Location = New System.Drawing.Point(327, 54)
+        Me.btnAbrir.Name = "btnAbrir"
+        Me.btnAbrir.Size = New System.Drawing.Size(75, 23)
+        Me.btnAbrir.TabIndex = 4
+        Me.btnAbrir.Text = "Abrir Cuenta"
+        Me.btnAbrir.UseVisualStyleBackColor = True
+        '
+        'btnDepositos
+        '
+        Me.btnDepositos.Location = New System.Drawing.Point(39, 21)
+        Me.btnDepositos.Name = "btnDepositos"
+        Me.btnDepositos.Size = New System.Drawing.Size(100, 30)
+        Me.btnDepositos.TabIndex = 0
+        Me.btnDepositos.Text = "Depósitos"
+        Me.btnDepositos.UseVisualStyleBackColor = True
+        '
+        'btnRetiros
+        '
+        Me.btnRetiros.Location = New System.Drawing.Point(221, 21)
+        Me.btnRetiros.Name = "btnRetiros"
+        Me.btnRetiros.Size = New System.Drawing.Size(100, 30)
+        Me.btnRetiros.TabIndex = 1
+        Me.btnRetiros.Text = "Retiros"
+        Me.btnRetiros.UseVisualStyleBackColor = True
+        '
+        'GroupBox3
+        '
+        Me.GroupBox3.Controls.Add(Me.btnLimpiar)
+        Me.GroupBox3.Controls.Add(Me.txtSaldo)
+        Me.GroupBox3.Controls.Add(Me.Label5)
+        Me.GroupBox3.Controls.Add(Me.lstRetiros)
+        Me.GroupBox3.Controls.Add(Me.lstDepositos)
+        Me.GroupBox3.Controls.Add(Me.Label4)
+        Me.GroupBox3.Controls.Add(Me.Label3)
+        Me.GroupBox3.Location = New System.Drawing.Point(13, 212)
+        Me.GroupBox3.Name = "GroupBox3"
+        Me.GroupBox3.Size = New System.Drawing.Size(440, 190)
+        Me.GroupBox3.TabIndex = 2
+        Me.GroupBox3.TabStop = False
+        Me.GroupBox3.Text = "Historial"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(26, 30)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(54, 13)
+        Me.Label3.TabIndex = 0
+        Me.Label3.Text = "Depósitos"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(190, 30)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(40, 13)
+        Me.Label4.TabIndex = 1
+        Me.Label4.Text = "Retiros"
+        '
+        'lstDepositos
+        '
+        Me.lstDepositos.FormattingEnabled = True
+        Me.lstDepositos.Location = New System.Drawing.Point(29, 64)
+        Me.lstDepositos.Name = "lstDepositos"
+        Me.lstDepositos.Size = New System.Drawing.Size(120, 95)
+        Me.lstDepositos.TabIndex = 2
+        '
+        'lstRetiros
+        '
+        Me.lstRetiros.FormattingEnabled = True
+        Me.lstRetiros.Location = New System.Drawing.Point(193, 64)
+        Me.lstRetiros.Name = "lstRetiros"
+        Me.lstRetiros.Size = New System.Drawing.Size(120, 95)
+        Me.lstRetiros.TabIndex = 3
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(324, 48)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(37, 13)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "Saldo:"
+        '
+        'txtSaldo
+        '
+        Me.txtSaldo.Location = New System.Drawing.Point(327, 75)
+        Me.txtSaldo.Name = "txtSaldo"
+        Me.txtSaldo.Size = New System.Drawing.Size(100, 20)
+        Me.txtSaldo.TabIndex = 5
+        '
+        'btnLimpiar
+        '
+        Me.btnLimpiar.Location = New System.Drawing.Point(341, 124)
+        Me.btnLimpiar.Name = "btnLimpiar"
+        Me.btnLimpiar.Size = New System.Drawing.Size(75, 23)
+        Me.btnLimpiar.TabIndex = 6
+        Me.btnLimpiar.Text = "Limpiar"
+        Me.btnLimpiar.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(470, 409)
+        Me.Controls.Add(Me.GroupBox3)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Form1"
+        Me.Text = "Gestión de Ahorros"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox3.ResumeLayout(False)
+        Me.GroupBox3.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents btnAbrir As Button
+    Friend WithEvents txtMonto As TextBox
+    Friend WithEvents txtCliente As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents btnRetiros As Button
+    Friend WithEvents btnDepositos As Button
+    Friend WithEvents GroupBox3 As GroupBox
+    Friend WithEvents btnLimpiar As Button
+    Friend WithEvents txtSaldo As TextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents lstRetiros As ListBox
+    Friend WithEvents lstDepositos As ListBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+End Class
---
+++ prGestionAhorros/Form1.vb
@@ -0,0 +1,85 @@
+﻿Public Class Form1
+    'Propiedad
+    Public Property Monto As Double
+
+    'Procedimientos
+    Private Sub ActivarControles()
+        txtCliente.Enabled = False
+        txtMonto.Enabled = False
+        btnAbrir.Enabled = False
+        txtSaldo.Enabled = True
+        btnRetiros.Enabled = True
+        btnDepositos.Enabled = True
+    End Sub
+
+    Private Sub DesactivarControles()
+        txtCliente.Enabled = True
+        txtMonto.Enabled = True
+        btnAbrir.Enabled = True
+        txtSaldo.Enabled = False
+        btnRetiros.Enabled = False
+        btnDepositos.Enabled = False
+    End Sub
+
+    Private Sub Mostrar()
+        txtSaldo.Text = Monto
+    End Sub
+
+    'Funciones
+    Private Function Leer(mensaje As String) As Double
+        Dim cantidad As Double
+        cantidad = InputBox("Ingrese monto a " + mensaje + ": ", "Gestión de Ahorros", "0", 100, 0)
+
+        Return cantidad
+    End Function
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        DesactivarControles()
+    End Sub
+
+    Private Sub BtnAbrir_Click(sender As Object, e As EventArgs) Handles btnAbrir.Click
+        Dim cliente As String
+        cliente = txtCliente.Text
+
+        Monto = txtMonto.Text
+
+        If (Monto >= 0) Then
+            ActivarControles()
+        Else
+            MessageBox.Show("El monto no puede ser inferior a cero", "Gestión de Ahorros", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End If
+    End Sub
+
+    Private Sub BtnDepositos_Click(sender As Object, e As EventArgs) Handles btnDepositos.Click
+        Dim deposito As Double
+        deposito = Leer("Depositar")
+        Monto += deposito
+
+        lstDepositos.Items.Add(deposito)
+        Mostrar()
+    End Sub
+
+    Private Sub BtnRetiros_Click(sender As Object, e As EventArgs) Handles btnRetiros.Click
+        Dim retiro As Double
+
+        retiro = Leer("Retirar")
+
+        If (retiro <= Monto) Then
+            Monto -= retiro
+            lstRetiros.Items.Add(retiro)
+            Mostrar()
+        Else
+            MessageBox.Show("No se puede retirar una cantidad mayor al monto actual", "Gestión de Ahorros", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End If
+    End Sub
+
+    Private Sub BtnLimpiar_Click(sender As Object, e As EventArgs) Handles btnLimpiar.Click
+        DesactivarControles()
+        lstDepositos.Items.Clear()
+        lstRetiros.Items.Clear()
+        txtMonto.Clear()
+        txtCliente.Clear()
+        txtSaldo.Clear()
+
+    End Sub
+End Class
